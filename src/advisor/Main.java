1cab06ff1c29167ba39fa7c961eef5a6b058bbf1
Ansis Maleckis <maleckisansis@gmail.com>
2022-08-20 11:40:00 +0300

    access token received

--- src/advisor/Main.java
+++ src/advisor/Main.java
@@ -6,16 +6,25 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
 import java.util.Scanner;
 
 public class Main {
 
-    private static final String port = "8000";
+    private static final String port = "8000"; //when changing the port a new authoriazation has to be made at Spotify
+    private static final String serverPath = "http://localhost:" + port;
     private final static String url = "https://accounts.spotify.com/authorize?client_id=e250b9f5fe2848f08f36f20b1274866a&redirect_uri=http://localhost:" + port + "&response_type=code";
-    private static String input = "";
+    private final static String clientId = "e250b9f5fe2848f08f36f20b1274866a";
+    private final static String clientSecret = "006e08a74dbd4d5caa1b5fdc8d247687";
+    private final static String clientIdSecret = clientId + ":" + clientSecret;
+    private static String code = "";
+    private static String accessToken = "";
 
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
 
         HttpServer server = startServer(port);
@@ -28,18 +37,33 @@ public class Main {
                 System.out.println("use this link to request the access code:");
                 System.out.println(url);
                 System.out.println("waiting for code...");
-                while(input.equals("")) {
+                while(code.equals("")) {
                     Thread.sleep(100);
                 }
                 server.stop(0);
                 System.out.println("code received");
                 System.out.println("making http request for access_token...");
-                System.out.println(input);
+                System.out.println(code);
+                HttpClient client = HttpClient.newBuilder().build();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .headers("Content-Type", "application/x-www-form-urlencoded", "Authorization", "Basic " + Base64.getEncoder().encodeToString(clientIdSecret.getBytes()))
+                        .uri(URI.create("https://accounts.spotify.com/api/token"))
+                        .POST(HttpRequest.BodyPublishers.ofString("client_id=" + clientId +
+                                                                    "&client_secret=" + clientSecret +
+                                                                    "&grant_type=authorization_code" +
+                                                                    "&code=" + code.split("=")[1] +
+                                                                    "&redirect_uri=" + serverPath))
+                        .build();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                System.out.println("RESPONSE body: " + response.body());
+                System.out.println("RESPONSE headers: " + response.headers());
+                System.out.println("RESPONSE status code: " + response.statusCode());
 
                 System.out.println("---SUCCESS---");
                 break;
             }
             else if(i.equals("exit")) {
+                scanner.close();
                 System.exit(0);
             }
             else
@@ -97,13 +121,19 @@ public class Main {
             server.createContext("/",
                     new HttpHandler() {
                         public void handle(HttpExchange exchange) throws IOException {
-                            exchange.sendResponseHeaders(200, "OK".length());
-                            exchange.getResponseBody().write("OK".getBytes());
-                            exchange.getResponseBody().close();
                             String response = exchange.getRequestURI().getQuery();
-                            if(response != null)
-                                input = response;
-                            System.out.println("input received at controller: " + input);
+                            if(response != null && response.startsWith("code=")) {
+                                code = response;
+                                exchange.sendResponseHeaders(200, "Got the code. Return back to your program.".length());
+                                exchange.getResponseBody().write("Got the code. Return back to your program.".getBytes());
+                                exchange.getResponseBody().close();
+                                System.out.println("code received at controller: " + code);
+                            }
+                            else {
+                                exchange.sendResponseHeaders(200, "Authorization code not found. Try again.".length());
+                                exchange.getResponseBody().write("Authorization code not found. Try again.".getBytes());
+                                exchange.getResponseBody().close();
+                            }
                         }
                     }
             );
